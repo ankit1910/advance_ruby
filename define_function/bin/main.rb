ed098599fc85909dcf2aa8bf59369edafc6246e4
ankit1910 <ankit1910@gmail.com>
2013-08-23 14:49:47 +0530

    define function

---
+++ define_function/bin/main.rb
@@ -0,0 +1,15 @@
+puts "Enter Method name you want to make"
+method_name = gets.chomp
+puts "Enter One line  code you want to evaluate"
+code = gets.chomp
+puts "output :-"
+begin
+  Object.send(:define_method, method_name.to_s) do
+    puts eval(code)
+  end
+  eval "#{method_name}"
+rescue StandardError => e
+  p e.message
+rescue ScriptError => e
+  p e.message  
+end
