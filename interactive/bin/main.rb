7b1ed58531e61d8c1a2c0601ff795b7536a5ed71
ankit1910 <ankit1910@gmail.com>
2013-08-26 10:41:01 +0530

    seprate class to eval code

--- interactive/bin/main.rb
+++ interactive/bin/main.rb
@@ -1,18 +1,16 @@
-while true  
-  begin
-    puts "Please enter the code you want to evaluate"
-    code = ""
-    while true
-      input = gets.chomp
-      break if input =~ /q/i or input.empty?
-      code << input
-      code << ';'
-    end
-    puts "output is"
-    puts eval(code)
-  rescue Exception
-    puts "You got something incorrect in you code"
+require_relative '../lib/code.rb'
+begin
+  puts "\nPlease enter the code you want to evaluate"
+  code = ""
+  while true
+    input = gets.chomp
+    break if input =~ /q/i or input.empty?
+    code << input + ';'
   end
-  puts "you want to execute more code press 'y' for yes"
-  break if gets !~ /y|yes/i
+  obj = Code.new
+  puts "your output is :"
+  puts obj.evaluate_code(code)
+rescue StandardError, SyntaxError
+  puts "You got something incorrect in you code"
+  retry
 end
---
+++ interactive/lib/code.rb
@@ -0,0 +1,5 @@
+class Code
+  def evaluate_code(code)
+    eval(code)
+  end
+end
