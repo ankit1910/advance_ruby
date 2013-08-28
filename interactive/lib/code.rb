ec3fdaae9ece5d8832e671fe50b863f3172e7c99
ankit1910 <ankit1910@gmail.com>
2013-08-28 10:32:09 +0530

    object oriented

--- interactive/lib/code.rb
+++ interactive/lib/code.rb
@@ -2,10 +2,12 @@ class Code
   def initialize 
     @source_code = ""
   end
+  
   def append(input)
     abort "program quited by user" if input =~ /^q$/i
     @source_code << input + ';'
   end
+
   def evaluate
     eval(@source_code)
   end
