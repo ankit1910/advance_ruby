a97823e9f5aabdcab8306ec9996acc15326de8f0
ankit1910 <ankit1910@gmail.com>
2013-08-30 10:31:41 +0530

    singleton class

--- singleton/bin/main.rb
+++ singleton/bin/main.rb
@@ -1,11 +1,21 @@
 require_relative '../lib/string.rb'
-instance1 = "hello world"
-instance2 = "HELLO WORLD"
+instance1 = instance2 = instance3 = "HELLO world"
 
+# first method direct
 def instance1.to_upcase
   upcase 
 end
 
-puts instance1.to_upcase
+# defining singleton class for instance3
+ghost = class << instance3 ; self ; end
+ghost.class_eval do
+  def reverse_string
+    reverse
+  end
+end
+
+# defining method for creating instance method in String class
 String.create_instance_method_for(instance2)
+puts instance1.to_upcase
 puts instance2.to_downcase
+puts instance3.reverse_string
